c6c8140a4abef49466053877c39b29869b16fad8
piumal96 <piumalkumara@live.com>
2022-08-03 09:52:51 +0530

    Added ststefull widget and count

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,34 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:testapp/pages/home.dart';
 
-class AboutArgument{
+class AboutArgument {
   final String name;
 
   AboutArgument({required this.name});
 }
 
-class About extends StatelessWidget {
+class About extends StatefulWidget {
   const About({Key? key}) : super(key: key);
-  static const routeName="/About";
+  static const routeName = "/About";
+
+  @override
+  State<About> createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  int count = 0;
+
+  void onIncrement() {
+    setState(() {
+      count++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final AboutArgument args = ModalRoute.of(context)?.settings.arguments as AboutArgument;
+    final AboutArgument args =
+        ModalRoute.of(context)?.settings.arguments as AboutArgument;
     return Scaffold(
       appBar: AppBar(
         title: Text("About Page"),
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.all(8),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("About Page ${args.name}",style: Theme.of(context).textTheme.headline3,),
-            OutlinedButton(onPressed: (){
-              Navigator.of(context).pushNamed(Home.routename);
-            }, child: Text("Back to Holme")),
+            SizedBox(
+              width: double.infinity,
+            ),
+            Text(
+              "About Page ${args.name}",
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            Text(
+              "The Count is :$count",
+              style: GoogleFonts.chilanka(fontSize: 38),
+            ),
+            OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(Home.routename);
+                },
+                child: Text("Back to Holme")),
           ],
         ),
       ),
-
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: onIncrement),
     );
   }
 }
